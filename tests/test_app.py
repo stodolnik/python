1dab1120db631787f6664c7793a97679db28b21e
Lucas Stodolnik Raineri <stodolnik@Lucass-MacBook-Pro.local>
2025-07-31 07:12:05 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello World!'}
