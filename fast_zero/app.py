1d6bd765c7ed384976ba505d5a1c4ac7b891aeb9
Lucas Stodolnik <lucas.raineri@gmail.com>
2025-08-02 09:10:10 -0300

    Implementando rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,67 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, Userpublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
-def read_rooot():
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=Userpublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def return_users():
+    return {'users': database}
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Userpublic
+)
+def users(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = database[user_id - 1]
+    return user_with_id
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Userpublic
+)
+def update_users(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_users(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,59 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
+from fast_zero.app import UserList, Userpublic, UserSchema
 
 
-def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_create_user(client):
+    user_input = UserSchema(
+        username='Lucas', email='lucas@example.com', password='teste'
+    )
+    user_output = Userpublic(id=1, username='Lucas', email='lucas@example.com')
+
+    response = client.post('/users/', json=user_input.model_dump())
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == user_output.model_dump()
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    user_output = UserList(
+        users=[Userpublic(id=1, username='Lucas', email='lucas@example.com')]
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_output.model_dump()
+
+
+def test_read_user(client):
+    user_output = Userpublic(id=1, username='Lucas', email='lucas@example.com')
+
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_output.model_dump()
+
+
+def test_update_user(client):
+    user_input = UserSchema(
+        username='Lucas', email='lucas@example.com', password='teste'
+    )
+    user_output = Userpublic(id=1, username='Lucas', email='lucas@example.com')
+
+    response = client.put('/users/1', json=user_input.model_dump())
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_output.model_dump()
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    user_output = {'message': 'User deleted'}
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_output
